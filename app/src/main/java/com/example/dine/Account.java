8add023bdfa044930b119d0428dc9cae9a042e5a
Lywen <lim.lywen15@gmail.com>
2024-11-12 00:20:49 +0700

    up

--- app/src/main/java/com/example/dine/Account.java
+++ app/src/main/java/com/example/dine/Account.java
@@ -2,21 +2,26 @@ package com.example.dine;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class account extends AppCompatActivity {
+public class Account extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        // Memuat fragment_account ke dalam FrameLayout
-        AccountFragment accountFragment = new AccountFragment();
+        // Memuat fragment_account.xml ke dalam FrameLayout
+        loadFragment(new AccountFragment());
+    }
+
+    // Method untuk memuat fragment
+    private void loadFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, accountFragment);
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/dine/AccountFragment.java
+++ app/src/main/java/com/example/dine/AccountFragment.java
@@ -1,19 +1,17 @@
 package com.example.dine;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
 
 public class AccountFragment extends Fragment {
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Menghubungkan dengan layout fragment_account.xml
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Pastikan Anda menggunakan layout yang benar, misalnya: fragment_account
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
+
