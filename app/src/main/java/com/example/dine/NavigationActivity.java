98c40c2722007452e4eba3ac9c68bd495c88f905
Stan <michel.stanley@student.president.ac.id>
2024-11-13 19:57:30 +0700

    Account button
    Implemented a function for account button within navbar as well as deletion of unnecessary files

--- app/src/main/java/com/example/dine/NavigationActivity.java
+++ app/src/main/java/com/example/dine/NavigationActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavigationActivity extends AppCompatActivity {
 
@@ -19,6 +23,29 @@ public class NavigationActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navbar);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                // Handle home button click
+                return true;
+            } else if (itemId == R.id.scanner) {
+                // Handle scanner button click
+                return true;
+            } else if (itemId == R.id.recipe) {
+                // Handle recipe button click
+                return true;
+            } else if (itemId == R.id.account) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment_account.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+                return true;
+            }
+            return false;
         });
     }
 }
