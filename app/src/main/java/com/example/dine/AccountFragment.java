2236df54edf9bf2f0faf883eb964814cad90eadf
Samuel Thomas <samuel.thomas@student.president.ac.id>
2024-11-15 13:48:31 +0700

    Adjust some UI and Element

--- app/src/main/java/com/example/dine/AccountFragment.java
+++ app/src/main/java/com/example/dine/AccountFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_account#newInstance} factory method to
+ * Use the {@link AccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_account extends Fragment {
+public class AccountFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class fragment_account extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_account() {
+    public AccountFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class fragment_account extends Fragment {
      * @return A new instance of fragment fragment_account.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_account newInstance(String param1, String param2) {
-        fragment_account fragment = new fragment_account();
+    public static AccountFragment newInstance(String param1, String param2) {
+        AccountFragment fragment = new AccountFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
--- app/src/main/java/com/example/dine/NavigationActivity.java
+++ app/src/main/java/com/example/dine/NavigationActivity.java
@@ -7,28 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.dine.databinding.ActivityNavigationBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavigationActivity extends AppCompatActivity {
-
+    private ActivityNavigationBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_navigation);
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         BottomNavigationView bottomNavigationView = findViewById(R.id.navbar);
+        binding.navbar.setSelectedItemId(R.id.home);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
-                // Handle home button click
+                
                 return true;
             } else if (itemId == R.id.scanner) {
                 // Handle scanner button click
@@ -37,15 +41,17 @@ public class NavigationActivity extends AppCompatActivity {
                 // Handle recipe button click
                 return true;
             } else if (itemId == R.id.account) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, fragment_account.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack("name")
-                        .commit();
+                replaceFragment(new AccountFragment());
                 return true;
             }
             return false;
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
